d3ea9fc700151effc8e4c5654b639bf765aaaf07
Martin del Rosario <delrosariomartin23@gmail.com>
2022-01-08 17:01:20 +0100

    agrego loader

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+// LOADER
+window.addEventListener('load',()=>{
+    $('.loader').delay(400).fadeOut('slow', ()=>{console.log('Finalizar loader')});
+    //MUESTRO EL CUERPO DE LA PAGINA
+    $('.cuerpo').delay(1000).fadeIn('slow',()=>{console.log('Body cargado')});
+})
+
 document.getElementById("calificacion").addEventListener("click", function(event){
     event.preventDefault()
 });
